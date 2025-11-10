0db9577d790411e7881ac2be4b9efb228c99112d
Alex Carmichael <carmichael.alexander@gmail.com>
2025-11-10 10:15:46 -0800

    some boilerplate code

--- calendar-service/src/app.module.ts
+++ calendar-service/src/app.module.ts
@@ -5,13 +5,14 @@ import { CalendarEntity } from './calendar/entities/calendar.entity'; // use wit
 import { CalendarModule } from './calendar/calendar.module';
 import { TypeOrmModule } from '@nestjs/typeorm'; // use with a real database
 import { UserModule } from './user/user.module';
+import { UserEntity } from './user/entities/user.entity'; // use with a real database
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: ':memory:',
-      entities: [CalendarEntity],
+      entities: [CalendarEntity, UserEntity],
       synchronize: true,
 }), CalendarModule, UserModule,
   ], 
--- calendar-service/src/user/entities/user.entity.ts
+++ calendar-service/src/user/entities/user.entity.ts
@@ -1,1 +1,17 @@
-export class User {}
+import { 
+    Entity,
+    Column,
+    PrimaryGeneratedColumn 
+} from 'typeorm';
+
+@Entity( {name: "user"} )
+export class UserEntity {
+ @PrimaryGeneratedColumn('uuid')
+  id?: string; // The ID will be a string type
+
+  @Column()
+  firstName!: string;
+
+  @Column()
+  lastName!: string;
+}
