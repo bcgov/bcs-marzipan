5d69c4f574a261b7d53e84c377303fc5ff403139
Alex Carmichael <carmichael.alexander@gmail.com>
2026-01-15 09:55:59 -0800

    removed redundant files

--- calendar-ui/src/App.tsx
+++ calendar-ui/src/App.tsx
@@ -13,22 +13,6 @@ import './styles/App.css';
 import DraftsPage from './pages/Drafts';
 import { CreateActivityForm } from './pages/CreateActivityForm';
 import { Settings } from './pages/Settings';
-import { Categories } from './pages/dataAdmin/Categories';
-import { Cities } from './pages/dataAdmin/Cities';
-import { CommunicationMaterials } from './pages/dataAdmin/CommunicationMaterials';
-import { GovernmentRepresentatives } from './pages/dataAdmin/GovernmentRepresentatives';
-import { HQTags } from './pages/dataAdmin/HQTags';
-import { Ministries } from './pages/dataAdmin/Ministries';
-import { NewsSubscribe } from './pages/dataAdmin/NewsSubscribe';
-import { NRDistributions } from './pages/dataAdmin/NRDistributions';
-import { NROrigins } from './pages/dataAdmin/NROrigins';
-import { PremierRequested } from './pages/dataAdmin/PremierRequested';
-import { Roles } from './pages/dataAdmin/Roles';
-import { Status } from './pages/dataAdmin/Status';
-import { SystemUsers } from './pages/dataAdmin/SystemUsers';
-import { Themes } from './pages/dataAdmin/Themes';
-import { UserAdmin } from './pages/dataAdmin/userAdmin';
-import { TransferActivities } from './pages/dataAdmin/transferActivities';
 
 function App() {
   return (
@@ -47,37 +31,6 @@ function App() {
           <Route path="/details" element={<EntryDetails />} />
           {/* Add more routes here */}
           <Route path="/settings" element={<Settings />} />
-          <Route path="/manage-users" element={<UserAdmin />} />
-          <Route path="/transfer-activities" element={<TransferActivities />} />
-          <Route path="/admin/lookup/categories" element={<Categories />} />
-          <Route path="/admin/lookup/cities" element={<Cities />} />
-          <Route
-            path="/admin/lookup/communication-materials"
-            element={<CommunicationMaterials />}
-          />
-          <Route
-            path="/admin/lookup/government-representatives"
-            element={<GovernmentRepresentatives />}
-          />
-          <Route path="/admin/lookup/hq-tags" element={<HQTags />} />
-          <Route path="/admin/lookup/ministries" element={<Ministries />} />
-          <Route
-            path="/admin/lookup/news-subscribe"
-            element={<NewsSubscribe />}
-          />
-          <Route
-            path="/admin/lookup/nr-distributions"
-            element={<NRDistributions />}
-          />
-          <Route path="/admin/lookup/nr-origins" element={<NROrigins />} />
-          <Route
-            path="/admin/lookup/premier-requested"
-            element={<PremierRequested />}
-          />
-          <Route path="/admin/lookup/roles" element={<Roles />} />
-          <Route path="/admin/lookup/status" element={<Status />} />
-          <Route path="/admin/lookup/system-users" element={<SystemUsers />} />
-          <Route path="/admin/lookup/themes" element={<Themes />} />
         </Route>
       </Routes>
     </FluentProvider>
