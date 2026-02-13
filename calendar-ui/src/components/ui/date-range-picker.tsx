02645ffc275e6f6a4787e2be9f06a081dfb8d0b4
cgovbc Chris Allen <chris.allen@gov.bc.ca>
2026-02-12 22:17:30 -0800

    fix(format): run prettier format

--- calendar-ui/src/components/ui/date-range-picker.tsx
+++ calendar-ui/src/components/ui/date-range-picker.tsx
@@ -1,6 +1,6 @@
-import * as React from 'react';
 import { format } from 'date-fns';
 import { Calendar as CalendarIcon } from 'lucide-react';
+import * as React from 'react';
 
 import { cn } from '../../lib/utils';
 import { Button } from './button';
