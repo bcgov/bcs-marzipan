53a8b3ea1e89f0cf083137c5a06275a730badaa6
cgovbc Chris Allen <chris.allen@gov.bc.ca>
2026-02-25 00:10:40 -0800

    style: refactor show more in activity table summary

--- calendar-ui/src/components/ActivityTable/ActivityTable.tsx
+++ calendar-ui/src/components/ActivityTable/ActivityTable.tsx
@@ -247,7 +247,7 @@ function SummaryCell({ row }: { row: ActivityTableRow }) {
 
         {isCollapsedWithTruncation && (
           <span
-            className="group-hover/bg-[linear-gradient(to_right,transparent_0%,slate-50_35%,slate-50_100%)] absolute right-0 bottom-0 w-28"
+            className="absolute right-0 bottom-0 w-28 group-hover/row:bg-[linear-gradient(to_right,transparent_0%,rgb(248_250_252/0.5)_35%,rgb(248_250_252/0.5)_100%)]"
             aria-hidden
           >
             <span className="flex justify-end bg-[linear-gradient(to_right,transparent_0%,white_35%,white_100%)] whitespace-nowrap [&>button]:inline">
