fa330cbe6774177698e084236a6f40f2a23c6c53
ShovelHand <carmichael.alexander@gmail.com>
2026-05-13 16:14:13 -0700

    refactor(ui): rename favourites to watchlist and swap star icon for eye

--- calendar-ui/src/pages/ActivityListPage.tsx
+++ calendar-ui/src/pages/ActivityListPage.tsx
@@ -264,7 +264,7 @@ export const ActivityListPage = () => {
             )}
             <TabsTrigger value="my-activities">My activities</TabsTrigger>
             <TabsTrigger value="shared-with-me">Shared with me</TabsTrigger>
-            <TabsTrigger value="favourites">Favourite activities</TabsTrigger>
+            <TabsTrigger value="favourites">My watchlist</TabsTrigger>
           </TabsList>
         </div>
 
