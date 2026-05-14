aa65628dfd4f5dce4ab09c02c2ec3869ecaea3be
ShovelHand <carmichael.alexander@gmail.com>
2026-05-14 16:50:26 -0700

    fix(CORPCAL-234): remove duplicate favourites tab trigger
    Favourite activities TabsTrigger (value=favourites) was left behind
    when the tab was renamed to My watchlist, causing both tabs to appear
    selected simultaneously.

--- calendar-ui/src/pages/ActivityListPage.tsx
+++ calendar-ui/src/pages/ActivityListPage.tsx
@@ -270,7 +270,6 @@ export const ActivityListPage = () => {
             )}
             <TabsTrigger value="my-activities">My activities</TabsTrigger>
             <TabsTrigger value="shared-with-me">Shared with me</TabsTrigger>
-            <TabsTrigger value="favourites">Favourite activities</TabsTrigger>
             <TabsTrigger value="assigned-to-me">Assigned to me</TabsTrigger>
             <TabsTrigger value="favourites">My watchlist</TabsTrigger>
           </TabsList>
